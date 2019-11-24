2a2c12c870953e779fe7976980e2ac2cd3c6a8ed
girin-dev <kw8799@gmail.com>
2019-11-24 20:51:10 +0900

    [Map] Naver 지도 연동

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const DivMap = styled.div`
+  height:400px;
+  width:70%;
+`;
 
 function Map() {
+  const [defaultCoords, setDefaultCoords] = useState({longitude:126.978433,latitude:37.566689});
+  const mapContainer = useRef();
+
+  useEffect(() => {
+		// console.log('컴포넌트가 화면에 나타남');
+    const { latitude, longitude } = defaultCoords;
+    const map = new window.naver.maps.Map(mapContainer.current, {
+        center: new window.naver.maps.LatLng(latitude, longitude),
+        zoom:11,
+    });
+    return () => {
+      // console.log('컴포넌트가 화면에서 사라짐');
+    };
+  }, []);
+  
+
   return (
     <>
-      개발을 시작하세요.
+      <DivMap
+        ref={mapContainer}
+      />
     </>
   );
 }
